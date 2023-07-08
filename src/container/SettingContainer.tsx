f6913ac1501a901f9ad6108e93454036385f32e5
JIWON KIM <kkjim1324@naver.com>
2023-07-08 17:44:33 +0900

    setting 추가

---
+++ src/container/SettingContainer.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material"
+import React, { useState, MouseEvent } from "react"
+
+function SettingContainer() {
+    return (
+        <Box>
+            
+        </Box>
+    )
+}
+
+export default SettingContainer
+
+
